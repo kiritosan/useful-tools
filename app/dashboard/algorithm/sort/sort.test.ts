56ac770ec949f163df1b5905c0baed92e7a72eb2
willem <1121567132@qq.com>
2023-12-23 12:13:27 +0800

    test: bubble sort test

--- app/dashboard/algorithm/sort/sort.test.ts
+++ app/dashboard/algorithm/sort/sort.test.ts
@@ -2,34 +2,23 @@ import { describe, it, expect } from 'vitest';
 import bubbleSort from './bubble';
 
 
-const originArr = [3,1,3,5,7,3]
+// const originArr = [3,1,3,5,7,3]
+const originArr = [1,2]
 
 describe('sort functions:', ()=> {
-// TODO: 檢驗兩個對象是同一個對象
   it('bubble sort: pure and correct', ()=> {
 
     const sortedToTestArr = bubbleSort(originArr);
     const sortedArr = originArr.slice().sort((a,b)=> a-b);
 
+    // check whether sort is correct
     expect(sortedToTestArr).toStrictEqual(sortedArr);
-    // expect(originArr).toBe(originArr);
+    // check whether the function is pure
+    expect(sortedToTestArr).not.toBe(originArr);
   });
 
   // it('xx sort', ()=> {
   //   expect(false).toBe(false);
-  // });
-
-  // it("common matchers test", () => {
-  //   let x = 20;
-  //   let obj1 = { name: "leon" };
-  //   let obj2 = { name: "leon" };
-  //   let obj3 = { name: "leon", age: undefined };
-
-  //   expect(20).toBe(20);
-  //   expect(obj1).not.toBe(obj2);
-  //   expect(obj1).toEqual(obj2);
-  //   expect(obj1).toEqual(obj3);
-  //   expect(obj1).not.toStrictEqual(obj3);
   // });
 
 });
