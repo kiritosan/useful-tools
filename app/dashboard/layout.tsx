0e6e2759e75adbb8cade6c4ea19c1ae61564b841
willem <1121567132@qq.com>
2023-12-23 13:56:18 +0800

    refactor: change router

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,32 +5,44 @@ import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 
 
+const buttonArr = ['reduxGenerator', 'algorithmViewer']
+
 export default function ReduxLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    const router = useRouter()
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const router = useRouter()
 
-    return (
-      <section className="px-16 py-8">
-        <header className="flex items-center justify-center">
-            <Button
-            type="primary"
-            onClick={() => router.push('/dashboard')}>
-              Back To Dashboard
-            </Button>
-        </header>
+  return (
+    <section className="px-16 py-8">
+      <header className="flex items-center justify-center">
+        <Button
+          type="primary"
+          onClick={() => router.push('/dashboard')}>
+          Back To Dashboard
+        </Button>
+      </header>
 
-        <nav className="px-24 my-4">
-          <Button type="primary">
-            <Link href="/dashboard/redux">redux</Link>
-          </Button>
-        </nav>
+      <nav className="px-24 my-4">
+        {
+          buttonArr.map((item, index) => {
+            return (
+              <Button
+                key={index}
+                type="primary"
+                className="ml-4"
+              >
+                <Link href={`/dashboard/${item}`}>{item}</Link>
+              </Button>
+            )
+          })
+        }
+      </nav>
 
-        <section>
-         {children}
-        </section>
+      <section>
+        {children}
       </section>
-    )
-  }
+    </section>
+  )
+}
