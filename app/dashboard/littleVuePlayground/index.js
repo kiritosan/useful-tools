86bfcda0b86264b54b0818280195e68bc8069d7d
willem <1121567132@qq.com>
2023-12-25 23:14:27 +0800

    feat: littleVue

---
+++ app/dashboard/littleVuePlayground/index.js
@@ -0,0 +1,11 @@
+import Vue from "../../../lib/smallVue/main";
+
+const options = {
+    el: "#app",
+    data: {
+        msg: "hello vue"
+    }
+}
+
+const vue = new Vue(options)
+console.log("🚀 ~ file: index.js:11 ~ vue:", vue)
