93f81637e92fc9a69f8c137a8cb1fa7b560840fe
zhangh-cs <zhangh-cs@glodon.com>
2024-01-02 17:12:52 +0800

    perf: 加入元素中心和鼠标位置的偏移量

--- app/dashboard/pointerEvents/page.tsx
+++ app/dashboard/pointerEvents/page.tsx
@@ -6,25 +6,35 @@ import './index.css';
 const pointerEvents = () => {
 
   const handler = (e) => {
+    let xDiff, yDiff
     const dom = document.querySelector('.moveBlock')
     if (dom && dom.style) {
       dom.style.position = 'absolute';
     }
     console.log("🚀 ~ file: page.tsx:30 ~ handler ~ dom?.style:", dom?.style?.position)
 
+
+    const rect = dom?.getBoundingClientRect()
+    if (dom) {
+      let centerX = rect.left + rect.width / 2
+      let centerY = rect.top + rect.height / 2
+      let x = e.clientX
+      let y = e.clientY
+      xDiff = x - centerX
+      yDiff = y - centerY
+    }
+
     const moveDom = (e) => {
       const rect = dom?.getBoundingClientRect()
-
 
       let x = e.clientX
       let y = e.clientY
 
-      if (dom && dom.style) {
-        dom.style.left = `${x - rect.width / 2}px`
-        dom.style.top = `${y - rect.height / 2}px`
-      }
 
-      console.log("🚀 ~ file: page.tsx:17 ~ moveDom ~ rect:", rect, x, y)
+      if (dom && dom.style) {
+        dom.style.left = `${x - rect.width / 2 - xDiff}px`
+        dom.style.top = `${y - rect.height / 2 - yDiff}px`
+      }
     }
 
     const onMove = (e) => {
