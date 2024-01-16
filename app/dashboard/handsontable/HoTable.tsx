1e732df139b7cca7212fa57cc49b670f27830fb0
zhangh-cs <zhangh-cs@glodon.com>
2024-01-16 10:44:26 +0800

    chore

--- app/dashboard/handsontable/HoTable.tsx
+++ app/dashboard/handsontable/HoTable.tsx
@@ -1,29 +1,44 @@
 import { HotTable } from '@handsontable/react';
+import { useEffect, useState } from 'react';
 // import 'handsontable/dist/handsontable.full.min.css';
 
-const HoTable = () => {
+let timerId: any;
 
-    const settings = {
-        data: [
-            ['', 'Tesla', 'Volvo', 'Toyota', 'Ford'],
-            ['2019', 10, 11, 12, 13],
-            ['2020', 20, 11, 14, 13],
-            ['2021', 30, 15, 12, 13]
-        ],
-        rowHeaders: true,
-        colHeaders: true,
-        width: "100%",
-        height: "auto",
-        autoWrapRow: true,
-        autoWrapCol: true,
-        licenseKey: "non-commercial-and-evaluation", // for non-commercial use only
-    }
+const HoTable = () => {
+    const [data, setData] = useState([
+        ['', 'Tesla', 'Volvo', 'Toyota', 'Ford'],
+        ['2019', 10, 11, 12, 13],
+        ['2020', 20, 11, 14, 13],
+        ['2021', 30, 15, 12, 13]
+    ]);
+
+    // useEffect(() => {
+    //     clearInterval(timerId)
+    //     timerId = setInterval(() => {
+    //         setData([...data])
+    //         console.log('1', 1)
+    //     }, 1 * 1000);
+    // }, []);
+
 
     return (
         <div>
             <HotTable
-                settings={settings}
-                stretchH='last'
+                data={data}
+                colHeaders={true}
+                rowHeaders={true}
+                width={'100%'}
+                height={'auto'}
+                stretchH={'last'}
+                licenseKey={'non-commercial-and-evaluation'}
+                autoWrapRow={true}
+                autoWrapCol={true}
+                afterChange={(changes, source) => {
+                    if (source !== 'loadData') {
+                        console.log('changes', changes);
+                        console.log("🚀 ~ HoTable ~ source:", source)
+                    }
+                }}
             />
         </div>
     );
