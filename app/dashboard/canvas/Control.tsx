f4992430ddc4fee5461863128cdc366b8938c2bb
zhangh-cs <zhangh-cs@glodon.com>
2024-01-18 16:57:32 +0800

    feat: add drawDoubleRect function

--- app/dashboard/canvas/Control.tsx
+++ app/dashboard/canvas/Control.tsx
@@ -7,9 +7,20 @@ const Control = ({ width, height }: { width: number; height: number }) => {
     (window as any).ctx.clearRect(0, 0, width, height);
   };
 
+  const drawDoubleRect = () => {
+    (window as any).ctx.fillStyle = "rgb(200,0,0)";
+    (window as any).ctx.fillRect(10, 10, 55, 50);
+
+    (window as any).ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    (window as any).ctx.fillRect(30, 30, 55, 50);
+  };
+
   return (
     <>
-      <Button onClick={onClear}>clear</Button>
+      <section className="flex space-x-4">
+        <Button onClick={onClear}>clear</Button>
+        <Button onClick={drawDoubleRect}>drawDoubleRect</Button>
+      </section>
     </>
   );
 };
