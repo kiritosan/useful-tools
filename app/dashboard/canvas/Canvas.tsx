e645835ff82a176e4fdf05aa951de5b4bbeb2b36
zhangh-cs <zhangh-cs@glodon.com>
2024-01-18 17:54:50 +0800

    feat: add drawStraightLine function

--- app/dashboard/canvas/Canvas.tsx
+++ app/dashboard/canvas/Canvas.tsx
@@ -4,10 +4,37 @@ import React, { useEffect, useRef, useState } from "react";
 
 const Canvas = ({ width, height }: { width: number; height: number }) => {
   const canvasRef = useRef(null);
+  const widthRef = useRef(width);
+  const heightRef = useRef(height);
+  console.log("widthP", width);
+  console.log("heightP", height);
+
+  const drawStraightLine = (e) => {
+    console.log(new Date());
+    const ctx = (window as any).ctx;
+    if (ctx) {
+      ctx.beginPath();
+      ctx.moveTo(0, 0);
+      ctx.lineTo(widthRef.current, heightRef.current);
+      ctx.stroke();
+    }
+  };
+
+  useEffect(() => {
+    widthRef.current = width;
+    heightRef.current = height;
+  }, [width, height]);
 
   useEffect(() => {
     if (canvasRef.current) {
       (window as any).ctx = canvasRef.current.getContext("2d");
+    }
+  }, []);
+
+  useEffect(() => {
+    // bind event of draw line
+    if (canvasRef.current) {
+      canvasRef.current.addEventListener("click", drawStraightLine);
     }
   }, []);
 
